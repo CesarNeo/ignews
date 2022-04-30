f6d070a22e02becafddd7be33042676e43f89baf
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-04-30 15:54:35 -0300

    Estilos globais do app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import { AppProps } from 'next/app'
 import Head from 'next/head'
+
+import { AppProps } from 'next/app'
+
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
